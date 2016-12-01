47c92aee82f7facd7c0ccda1fba1ecd2513c02c1
mnmpdadish <mnmpdadish@hotmail.com>
2016-12-01 17:26:43 -0500

    Changed main() to main0() to be compatible with python2.4 (on Mammoth - series).

--- manual_python2.7/example1_hello/step3/example1.c
+++ manual_python2.7/example1_hello/step3/example1.c
@@ -1,7 +1,7 @@
 #include "Python.h"
 #include <stdio.h>
 
-int main(void) {
+int main0(void) { //some version of python do not like if we keep the main() function in the .so
   printf("Hello World.\n");
   return 0;
 }
@@ -9,7 +9,7 @@ int main(void) {
 static PyObject * main_PythonWrapper(
        PyObject *self, PyObject *args){
     
-    main();
+    main0();
     return Py_BuildValue("i", 56);
 }
 
